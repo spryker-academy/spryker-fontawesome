c9e1e50e29bb7a247dcef99e4cc6ab431f064b49
Hidran Arias <hidran.arias@spryker.com>
2024-08-06 19:26:13 +0200

    Adds fontawesome icons

--- src/Pyz/Yves/ShopUi/Theme/default/templates/page-layout-main/index.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/templates/page-layout-main/index.ts
@@ -1,1 +1,8 @@
 import './page-layout-main.scss';
+// Import Font Awesome
+
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+import '@fortawesome/fontawesome-free/scss/solid.scss';
+import '@fortawesome/fontawesome-free/scss/brands.scss';
+import '@fortawesome/fontawesome-free/scss/regular.scss';
+
